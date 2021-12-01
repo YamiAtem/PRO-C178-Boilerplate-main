adc8cad2a1d54186a31a2a1c407dfbd7aefdd211
Atharva Mishra <63883108+YamiAtem@users.noreply.github.com>
2021-12-01 18:33:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let latitude = 22.7868542;
+let longitude = 88.3643296;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicHlkZXYxOSIsImEiOiJja3duaWo1cGwybXI4Mm9xaWY3dGltcGViIn0.Tf-AA38yRUtOej_v7thYpQ';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [latitude, longitude],
+    zoom: 16
+});
+
+map.addControl(
+	new mapboxgl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true
+		},
+		trackUserLocation: true
+	})
+);
+
+map.addControl(
+	new MapboxDirections({
+		accessToken: mapboxgl.accessToken
+	}),
+	'top-left'
+);
